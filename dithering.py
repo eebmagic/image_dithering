c62235f319004a143679e8a24ca3df3e6a246732
Ethan B <eebmagic@gmail.com>
2019-08-12 11:36:03 -0500

    now saves images as 1-bit instead of RGB

--- dithering.py
+++ dithering.py
@@ -1,39 +1,63 @@
 from PIL import Image
 from statistics import mean
 from pathlib import Path
-from tqdm import tqdm 
+from tqdm import tqdm
+import sys
+import os
 
-black = (0, 0, 0)
-white = (255, 255, 255)
-dithers = {0:[[black]*2]*2, 63:[[black, black],[black, white]], 127:[[black, white],[white, black]], 191:[[black, white],[white, white]], 255:[[white]*2]*2}
+# Get input image from command start
+image_PATH = None
+for i in sys.argv[1:]:
+	try:
+		im = Image.open(i).convert("LA")
+		image_PATH = i
+	except OSError:
+		pass	
+
+# Get output name from command start
+if '-o' in sys.argv[1:]:
+	fileName = sys.argv[sys.argv[1:].index('-o') + 2]
+	if not fileName.endswith('.png'):
+		fileName += '.png'
+else:
+	fileName = None
+
+# Get input image if none given yet
+if not image_PATH:
+	image_PATH = input("\n\tDrag file here: ").strip()
+	im = Image.open(image_PATH).convert("LA")
+
+# Convert image to raw RGB
+rawdata = [x[0] for x in list(im.getdata())]
+
+# Configurations for dithering patterns
+# Based on outline in evample_images/scale_guide.png
+black = 0
+white = 1
+dithers = {0: [[black] * 2] * 2, 63: [[black, black], [black, white]], 127: [[black, white], [white, black]], 191: [[black, white], [white, white]], 255: [[white] * 2] * 2}
 
 straight_options = [0, 63, 127, 191, 255]
 
-image_PATH = input("\n\tDrag file here: ").strip()
-
-im = Image.open(image_PATH)
-rawdata = list(im.getdata())
-
-averages = []
-print("\nGetting Averages...")
-for point in tqdm(rawdata):
-	averages.append(int(mean((point[0], point[1], point[2]))))
-
-MIN = int(min(averages))
-MAX = int(max(averages))
+# Use 
+MIN = int(min(rawdata))
+MAX = int(max(rawdata))
 MID = int(mean([MIN, MAX]))
 
-options = [MIN, int(mean([MIN, MID])), MID, int(mean([MID, MAX])), MAX]
+intervals = [MIN, int(mean([MIN, MID])), MID, int(mean([MID, MAX])), MAX]
+seg_distances = []
+for i in range(len(intervals) - 1):
+	seg_distances.append(intervals[i + 1] - intervals[i])
+min_jump_distance = min(seg_distances)
 
 greys = []
-print("\nMaking greyscale...")
-for avg in tqdm(averages):
+print("\nRounding grayscale values to intervals...")
+for avg in tqdm(rawdata):
 	distances = {}
-	for seg in options:
+	for seg in intervals:
 		distances[abs(avg - seg)] = seg
 
 	closest_fit = distances[min(distances)]
-	selection = straight_options[options.index(closest_fit)]
+	selection = straight_options[intervals.index(closest_fit)]
 	greys.append(selection)
 
 
@@ -42,6 +66,7 @@ newOne = []
 newTwo = []
 print("\nMaking new Image...")
 for ind, val in tqdm(enumerate(greys)):
+	# If at end of row then reset
 	if ind % im.size[0] == 0:
 		ditheredOut += (newOne + newTwo)
 		newOne = []
@@ -49,16 +74,18 @@ for ind, val in tqdm(enumerate(greys)):
 	newOne += (dithers[val][0])
 	newTwo += (dithers[val][1])
 
-newRes = (im.size[0]*2, im.size[1]*2)
-newIm = Image.new("RGB", newRes)
+newRes = (im.size[0] * 2, im.size[1] * 2)
+newIm = Image.new("1", newRes)
 newIm.putdata(ditheredOut)
 
-fileName = input("\n\tWhat do you want to name the output file?: ")
-if fileName.endswith('.png'):
-	pass
-else:
-	fileName += '.png'
+if not fileName:
+	fileName = input("\n\tWhat do you want to name the output file?: ")
+	if fileName.endswith('.png'):
+		pass
+	else:
+		fileName += '.png'
+print(f"Filename: {fileName}")
+savePath = os.getcwd() + '/' + fileName
+newIm.save(savePath)
+print(f"\nImage saved to: {savePath}")
 
-newIm.save(str(Path.home()) + "/Downloads/" + fileName)
-
-print("\nFile has been saved to ~/Downloads/\n")
